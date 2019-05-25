81720449c96834fb0e5b21c471214fad8cc09e64
Diane Litan <DLitan@kmc.int>
2019-05-25 16:32:08 +0800

    Updates

---
+++ precache-manifest.c4166c599e046b66345e474bf9b5a597.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afbcca2966b612ae2f3c41856a83973",
+    "url": "/reactfirstproject/index.html"
+  },
+  {
+    "revision": "9ee7f77723dfb25ddf9e",
+    "url": "/reactfirstproject/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4040a9e3650a719b8f7d",
+    "url": "/reactfirstproject/static/js/2.19f4c489.chunk.js"
+  },
+  {
+    "revision": "9ee7f77723dfb25ddf9e",
+    "url": "/reactfirstproject/static/js/main.ba617bbc.chunk.js"
+  },
+  {
+    "revision": "f48d3754f2b45465ebcc",
+    "url": "/reactfirstproject/static/js/runtime~main.e3961d6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactfirstproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
